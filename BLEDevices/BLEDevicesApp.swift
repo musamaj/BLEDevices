01c5451e6f28d6353b02a7ca10b86aa2198f3801
Usama Jamil <usama_csuet@hotmail.com>
2025-07-18 00:52:19 -0400

    Initial Commit

---
+++ BLEDevices/BLEDevicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEDevicesApp.swift
+//  BLEDevices
+//
+//  Created by Usama Jamil on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct BLEDevicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
